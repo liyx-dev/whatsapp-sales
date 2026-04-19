889b3cae8221cfb6553f77d0a9a2521a96206f4c
liyx-dev <goddayojoka22@gmail.com>
2026-04-19 15:08:34 +0100

    Update and rename .gitkeep to config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  supabaseUrl: "YOUR_SUPABASE_URL",
+  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY",
+
+  cloudflareR2BaseUrl: "YOUR_R2_PUBLIC_URL",
+
+  defaultWhatsapp: "27XXXXXXXXX",
+
+  appName: "WhatsApp Sales System"
+};
