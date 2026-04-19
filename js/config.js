5ee9ff27494a68cea9b8fa4a3875025dbdf9da4d
liyx-dev <goddayojoka22@gmail.com>
2026-04-19 15:42:54 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,7 @@
 const CONFIG = {
-  supabaseUrl: "YOUR_SUPABASE_URL",
-  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY",
-
-  cloudflareR2BaseUrl: "YOUR_R2_PUBLIC_URL",
-
-  defaultWhatsapp: "27XXXXXXXXX",
-
+  supabaseUrl: "https://ugffezktrojjhfbaxrrq.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnZmZlemt0cm9qamhmYmF4cnJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2ODg3NzIsImV4cCI6MjA5MTI2NDc3Mn0.gzFuLSj225QRnxdwyrH25Xpe1YZqPiK7fp_nrsETsW8",
+  cloudflareR2BaseUrl: "https://pub-0fc5736899f3449d987d356eafdca873.r2.dev",
+  defaultWhatsapp: "2347038268136",
   appName: "WhatsApp Sales System"
 };
